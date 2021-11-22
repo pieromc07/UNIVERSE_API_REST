5bca9a6f381092c83b664bfe7d1bcc69dbc042fa
pieromc07 <piero0716.mc@gmail.com>
2021-11-22 14:51:28 -0500

    add vercel file

--- src/index.js
+++ src/index.js
@@ -13,4 +13,7 @@ app.use(express.json());
 app.use(router);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+app.set('port', PORT);
+app.listen(app.get('port'), () => {
+    console.log(`Server is running on port ${app.get('port')}`);
+});
