7b522e54b51e2a053fba55591c6260146b69bf3a
Pieromc <piero0716.mc@gmail.com>
2022-02-18 11:14:44 -0500

    verify

--- src/controller/mail/mailController.js
+++ src/controller/mail/mailController.js
@@ -22,4 +22,30 @@ const sendMail = (req, res) => {
             res.status(500).json( { message : 'Error sending email', res : error });
         });
 }
-module.exports = {sendMail };
+
+
+const servipacMail = (req, res) => {
+
+    const { name, email, message} = req.body;
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+    const msg = {
+        from: email,
+        to: 'tg.elias.s@gmail.com',
+        subject: `Servipac - ${name}`,
+        text: message,
+    }
+    sgMail
+        .send(msg)
+        .then(() => {
+            console.log('Email sent')
+            res.status(200).json({message: 'Email sent'});
+        })
+        .catch((error) => {
+            console.error(error)
+            res.status(500).json( { message : 'Error sending email', res : error });
+        });
+}
+
+
+module.exports = {sendMail, servipacMail};
--- src/routes/mail/mail.routes.js
+++ src/routes/mail/mail.routes.js
@@ -1,8 +1,9 @@
 const { Router } = require('express');
-const { sendMail } = require('../../controller/mail/mailController');
+const { sendMail, servipacMail } = require('../../controller/mail/mailController');
 const sendEmail = Router();
 
 
 sendEmail.post('/send', sendMail);
+sendEmail.post('/servipac/send', servipacMail);
 
 module.exports = sendEmail;
